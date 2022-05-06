f2848a35cf526d4e80ceb98b9cdeec5eafd5c766
tangminjie <tangminjie_130@163.com>
2022-05-06 13:14:14 +0800

    update reademe and hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: "./hardhat-tutorial.env" });
+
+const RINKEBY_API_KEY_URL = process.env.RINKEBY_API_KEY_URL;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
+const ROPSTEN_API_KEY_URL = process.env.ROPSTEN_API_KEY_URL;
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+
+const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -17,5 +26,38 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.0"
+      },
+      {
+        version: "0.6.6",
+        settings: {}
+      },
+    ]
+  },
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+      //gasPrice: 125000000000,//you can adjust gasPrice locally to see how much it will cost on production
+      /*
+        notice no mnemonic here? it will just use account 0 of the hardhat node to deploy
+        (you can put in a mnemonic here to set the deployer locally)
+      */
+    },
+    rinkeby:{
+      url: RINKEBY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+    ropsten:{
+      url: ROPSTEN_API_KEY_URL,
+      accounts: [ROPSTEN_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey:ETHERSCAN_KEY,
+  },
 };
